599b0009eab248a499749c6d3d5fa84ae6546c0b
Daniel Esquivel <danielesquivel00.lded@gmail.com>
2023-12-03 00:52:27 -0600

    ADD Metodo para extraer la lista de personajes

--- src/helpers/radiata_scraper.rs
+++ src/helpers/radiata_scraper.rs
@@ -1,4 +1,6 @@
-use scraper::Html;
+use std::collections::HashMap;
+
+use scraper::{Html, Selector};
 
 pub struct RadiataScraper {
     url: String,
@@ -29,6 +31,41 @@ impl RadiataScraper {
 
     pub fn get_page(&self) -> &Html {
         &self.page
+    }
+
+    fn set_page(&mut self, new_page: Html) {
+        self.page = new_page;
+    }
+
+    pub fn get_list_characters(&self, html_tag: Option<&str>, html_class: Option<&str>) -> HashMap<String, String> {
+
+        let html_tag = html_tag.unwrap_or("li");
+        let html_class = html_class.unwrap_or(".category-page__member");
+
+        let selector_string = html_tag.to_owned() + html_class;
+
+        let selector = Selector::parse(&selector_string.to_owned()).unwrap();
+
+        let html_characters_list = self.page.select(&selector);
+
+        let mut character_list : HashMap<String, String> = HashMap::new();
+
+        for html_character in html_characters_list {
+            let link = html_character
+                .select(&Selector::parse("a").unwrap())
+                .next()
+                .map(|a| {
+                    let href = a.value().attr("href").unwrap().to_string();
+                    let text = a.value().attr("title").unwrap().to_string();
+                    (href, text)
+                });
+            
+            if let Some((href, text)) = link {
+                character_list.insert(href, text);
+            }
+        }
+        character_list
+        
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 mod models;
 mod helpers;
+use crate::helpers::radiata_scraper::RadiataScraper;
+
 
 fn main() {
-    println!("Hola mundo!");
+    let url = "https://radiata.fandom.com/wiki/Category:Recruitable_Characters";
+    let mut scrap = RadiataScraper::new(url.to_string());
+
+    println!("URL Actual: {}", scrap.get_url());
+
+    let list = scrap.get_list_characters(Some("li"),Some(".category-page__member"));
+
+    for (link, nombre) in list {
+        println!("{nombre}: {link}")
+    }
+
 }
