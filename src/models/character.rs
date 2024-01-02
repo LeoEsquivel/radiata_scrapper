2505deeab3206b477c9d0d07c88ba24de5985a7c
Daniel Esquivel <danielesquivel00.lded@gmail.com>
2024-01-01 21:37:50 -0600

    Metodo Display para RecuitmentInfo, fix nombre RecruimentInfo > RecruitmentInfo

--- src/models/character.rs
+++ src/models/character.rs
@@ -17,22 +17,30 @@ impl fmt::Display for Path {
     }
 }
 
-pub struct RecruimentInfo {
+pub struct RecruitmentInfo {
     pub requirements: Vec<String>,
     pub directions: Vec<String>,
+}
+
+impl fmt::Display for RecruitmentInfo {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let requirements = self.requirements.join(", ");
+        let directions = self.directions.join(", ");
+        write!(f, "Requirements: {}, Directions: {}", requirements, directions)
+    }
 }
 
 pub struct Character {
     pub name        : String,
     pub path        : Path,
-    pub recruitment : RecruimentInfo,
+    pub recruitment : RecruitmentInfo,
     pub image       : String,
 }
 
 impl Character {
 
     pub fn new(name: String, path: Path, image: String, requirements: Vec<String>, directions: Vec<String>) -> Character {
-        let recruitment_info = RecruimentInfo {
+        let recruitment_info = RecruitmentInfo {
             requirements,
             directions,
         };
@@ -49,24 +57,29 @@ impl Character {
         &self.name
     }
 
-    pub fn set_name(&mut self, new_name: String) {
-        self.name = new_name;
-    }
+    // pub fn set_name(&mut self, new_name: String) {
+    //     self.name = new_name;
+    // }
 
     pub fn get_path(&self) -> &Path {
         &self.path
     }
 
-    pub fn set_path(&mut self, new_path: Path) {
-        self.path = new_path
-    }
+    // pub fn set_path(&mut self, new_path: Path) {
+    //     self.path = new_path
+    // }
 
-    pub fn get_recruitment(&self) -> &RecruimentInfo {
+    pub fn get_recruitment(&self) -> &RecruitmentInfo {
         &self.recruitment
     }
 
-    pub fn set_recruitment(&mut self, new_recruitment: RecruimentInfo) {
-        self.recruitment = new_recruitment
+    // pub fn set_recruitment(&mut self, new_recruitment: RecruitmentInfo) {
+    //     self.recruitment = new_recruitment
+    // }
+
+    pub fn get_image(&self) -> &String {
+        &self.image
     }
+
 
 }
